f310848621b75871820f6f3cda27409e4bb3b5f5
JaeyeonKIM <foodsksms@gmail.com>
2020-03-20 01:03:18 +0900

    2020-3-20 spring study

--- src/main/java/me/jimmy/springmvc/DemoApplication.java
+++ src/main/java/me/jimmy/springmvc/DemoApplication.java
@@ -1,12 +1,18 @@
 package me.jimmy.springmvc;
 
 import org.apache.naming.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.support.GenericApplicationContext;
+import outer.MyService;
+
+import java.util.function.Supplier;
 
 @SpringBootApplication
 public class DemoApplication {
-	BeanFactory
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/me/jimmy/springmvc/book/JimmyBookRepository.java
+++
@@ -1,7 +0,0 @@
-package me.jimmy.springmvc.book;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class JimmyBookRepository implements BookRepository {
-}
