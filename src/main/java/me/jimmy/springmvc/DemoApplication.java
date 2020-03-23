dcbdfd5c7be253e3e39a2bafa52d20d1011cf312
JaeyeonKIM <foodsksms@gmail.com>
2020-03-23 12:00:16 +0900

    JPA start

--- src/main/java/me/jimmy/springmvc/DemoApplication.java
+++ src/main/java/me/jimmy/springmvc/DemoApplication.java
@@ -1,17 +1,31 @@
 package me.jimmy.springmvc;
 
 import org.apache.naming.factory.BeanFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-		SpringApplication app = new SpringApplication(DemoApplication.class);
-		app.setWebApplicationType(WebApplicationType.NONE);
-		app.run(args);
+	public static void main(String[] args) throws SQLException {
+		SpringApplication.run(DemoApplication.class, args);
+		String url = "jdbc:postgresql://localhost:54320/jpa-test";
+		String username = "id";
+		String password="test";
+		try (Connection connection = DriverManager.getConnection(url, username, password)){
+			System.out.println("Connection created: " + connection);
+			String sql = "insert into ACCOUNT values (1, 'docker', '123')";
+			try(PreparedStatement statement = connection.prepareStatement(sql)) {
+				statement.execute();
+			}
+		}
 	}
 }
