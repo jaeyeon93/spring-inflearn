5700eaf1d763b1d60b1433461104c7f7b9ca5563
JaeyeonKIM <foodsksms@gmail.com>
2020-03-25 01:08:01 +0900

    [JPA] 초기세팅
    - 프로젝트 초기세팅

--- src/main/java/me/jimmy/springmvc/DemoApplication.java
+++ src/main/java/me/jimmy/springmvc/DemoApplication.java
@@ -5,27 +5,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		String url = "jdbc:postgresql://localhost:54320/jpa-test";
-		String username = "id";
-		String password="test";
-		try (Connection connection = DriverManager.getConnection(url, username, password)){
-			System.out.println("Connection created: " + connection);
-			String sql = "insert into ACCOUNT values (1, 'docker', '123')";
-			try(PreparedStatement statement = connection.prepareStatement(sql)) {
-				statement.execute();
-			}
-		}
 	}
 }
